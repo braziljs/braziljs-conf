ab87f80f41f930c14f0a27cd58df2b13ae04547c
Jaydson Gomes <jayalemao@gmai.com>
2013-08-23 18:00:49 -0300

    first 2014 commit

---
+++ themes/yellow-swan/js/main.js
@@ -0,0 +1,38 @@
+// 21 Dez 2012, 4:42
+;(function ($, window, document, undefined) {
+    'use strict';
+    ({
+        animateScroll : function () {
+            $("#nav").find('.nav-link').on('click', function (event) {
+
+                var $this = $(this),
+                    $htmlBody = $('html, body'),
+                    linkTarget = $this.attr('href'),
+                    offSetTop;
+
+                // If not start with #, stop here!
+                if (linkTarget[0] !== '#') {
+                    return false;
+                }
+
+                event.preventDefault();
+
+                // Get distance of top
+                offSetTop = $(linkTarget).offset().top;
+
+                // Animate the scroll
+                $htmlBody.stop().animate({scrollTop : offSetTop}, function () {
+                    location.hash = linkTarget;
+                });
+            });
+        },
+
+        init : function () {
+            var that = this;
+
+            $(function () {
+                that.animateScroll();
+            });
+        }
+    }).init();
+}(jQuery, window, document));
