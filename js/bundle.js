fdc8fc257ed8cdc576aee4ef164eeb5a62803f51
Jaydson Gomes <jayalemao@gmail.com>
2015-07-08 22:53:27 -0300

    prevent default on speaker click. Other small fix

--- js/bundle.js
+++ js/bundle.js
@@ -401,46 +401,6 @@
 //let x = 'x';
 //console.log(util);
 
-//Overlay Menu
-(function () {
-	var triggerBttn = document.getElementById("trigger-overlay"),
-	    overlay = document.querySelector("div.overlay"),
-	    closeBttn = overlay.querySelector("button.overlay-close");
-	var transEndEventNames = {
-		"WebkitTransition": "webkitTransitionEnd",
-		"MozTransition": "transitionend",
-		"OTransition": "oTransitionEnd",
-		"msTransition": "MSTransitionEnd",
-		"transition": "transitionend"
-	},
-	    transEndEventName = transEndEventNames[Modernizr.prefixed("transition")],
-	    support = { transitions: Modernizr.csstransitions };
-
-	function toggleOverlay() {
-		if (classie.has(overlay, "open")) {
-			classie.remove(overlay, "open");
-			classie.add(overlay, "close");
-			var onEndTransitionFn = function onEndTransitionFn(ev) {
-				if (support.transitions) {
-					if (ev.propertyName !== "visibility") return;
-					this.removeEventListener(transEndEventName, onEndTransitionFn);
-				}
-				classie.remove(overlay, "close");
-			};
-			if (support.transitions) {
-				overlay.addEventListener(transEndEventName, onEndTransitionFn);
-			} else {
-				onEndTransitionFn();
-			}
-		} else if (!classie.has(overlay, "close")) {
-			classie.add(overlay, "open");
-		}
-	}
-
-	triggerBttn.addEventListener("click", toggleOverlay);
-	closeBttn.addEventListener("click", toggleOverlay);
-})();
-
 var enableKeyboardControls = false;
 
 $(document).ready(function () {
@@ -558,6 +518,7 @@ $(document).ready(function () {
 	}
 
 	function focusSpeakerEvt(event) {
+		event.preventDefault();
 		focusSpeaker(this);
 		//event.stopPropagation();
 		//event.preventDefault();
